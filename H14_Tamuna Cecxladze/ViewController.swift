6a1768ca9b748d71e39fc07c67321cd77e82a9ee
tamuna-cecxladze <56476680+tamuna-cecxladze@users.noreply.github.com>
2022-07-09 13:32:51 +0400

    Initial Commit

---
+++ H14_Tamuna Cecxladze/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  H14_Tamuna Cecxladze
+//
+//  Created by APPLE on 09.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
